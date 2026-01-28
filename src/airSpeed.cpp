c9011aecf44a1b797388d9c57a1af687e9c6b3b8
Shervin Naini <shervinnaini@shervins-air-2.ads.northwestern.edu>
2026-01-27 20:27:31 -0600

    Liveshare and stuff

--- src/airSpeed.cpp
+++ src/airSpeed.cpp
@@ -6,22 +6,21 @@
 
 class AirSpeed_Sensor_Pair {
 public: 
-    // Constructor to set the specific CS pin
-    AirSpeed_Sensor_Pair(int cs_pin) : cs_pin_(cs_pin) {}
+    // Constructor to set the specific ADC channel pin
+    AirSpeed_Sensor_Pair(int adc_pin_num) : adc_pin_num_(adc_pin_num) {}
 
     // Initialize pins - call this in setup()
     void init() {
-        pinMode(cs_pin_, OUTPUT);
-        digitalWrite(cs_pin_, HIGH);  // HIGH to disable
+        pinMode(adc_pin_num_, INPUT);  // Configure ADC pin as input
         SPI.begin();
     }
 
     uint16_t read_airSpeed_adc() {
         uint16_t data;
         SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE3));
-        digitalWrite(cs_pin_, LOW);  // LOW to enable
+        digitalWrite(AS_SPI_CS, LOW);  // LOW to enable
         data = SPI.transfer16(0x0000);
-        digitalWrite(cs_pin_, HIGH);  // HIGH to disable
+        digitalWrite(AS_SPI_CS, HIGH);  // HIGH to disable
         SPI.endTransaction();
         return data;
     }
@@ -32,12 +31,17 @@ public:
     }
 
 private:
-    const int cs_pin_;  // Specific CS pin for this instance
+    const int adc_pin_num_;  // Specific ADC pin number for this instance
 
     const float pressuremin_ = 0.0;    // min pressure (PSI)
     const float pressuremax_ = 25.0;   // max pressure (PSI)
     const float outputmin_ = 1638.0;   // min output (counts)
     const float outputmax_ = 14745.0;  // max output (counts)
+
+    // Default SPI pins (shared)
+    static const int AS_SPI_CS = 5;
+    static const int AS_SPI_MISO = 18;
+    static const int AS_SPI_SCLK = 19;
 };
 
 #endif // __AIRSPEED_H__
